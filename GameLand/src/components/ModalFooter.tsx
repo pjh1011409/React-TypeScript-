04c28e72f4873b8e035ae0a57f7ddfafbea8c2a8
ParkJeongHo <pjh31208@naver.com>
2023-01-05 14:11:56 +0900

    chore : Ready to Deploy

--- GameLand/src/components/ModalFooter.tsx
+++ GameLand/src/components/ModalFooter.tsx
@@ -7,7 +7,7 @@ const ModalFooter = (props: ModalFooter) => {
     <>
       <div className="flex items-center justify-center rounded-b border-t border-solid border-slate-200 p-6">
         <button
-          className="mx-5 mr-1 mb-1 rounded bg-[#00917a] px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-[#00917a]"
+          className=" mr-1 mb-1 rounded bg-[#00917a] px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-[#00917a]"
           type="button"
           onClick={() => props.setShowModal(false)}
         >
