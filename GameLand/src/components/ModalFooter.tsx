652b2c5259f834a7dd742c22927b99c25acd4b5a
ParkJeongHo <pjh31208@naver.com>
2023-01-05 14:32:33 +0900

    fix : Error routing with refresh browser

--- GameLand/src/components/ModalFooter.tsx
+++ GameLand/src/components/ModalFooter.tsx
@@ -7,7 +7,48 @@ const ModalFooter = (props: ModalFooter) => {
     <>
       <div className="flex items-center justify-center rounded-b border-t border-solid border-slate-200 p-6">
         <button
-          className=" mr-1 mb-1 rounded bg-[#00917a] px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-[#00917a]"
+          className=" mr-1 mb-1 rounded bg-[#ffc762] px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-[#00917a]"
+          type="button"
+          onClick={() => {
+            props.setShowModal(false);
+
+            {
+              props.gugudan === '구구단' && window.location.replace('/gugudan');
+            }
+            {
+              props.wordRelay === '끝말잇기' &&
+                window.location.replace('/word-relay');
+            }
+            {
+              props.baseball === '숫자야구' &&
+                window.location.replace('/number-baseball');
+            }
+            {
+              props.response === '반응속도체크' &&
+                window.location.replace('/response-check');
+            }
+            {
+              props.rsp === '가위바위보' &&
+                window.location.replace('/rock-scissors-paper');
+            }
+            {
+              props.lotto === '로또뽑기' &&
+                window.location.replace('/lotto-generator');
+            }
+            {
+              props.mine === '지뢰찾기' &&
+                window.location.replace('/mine-search');
+            }
+            {
+              props.tictactoe === '틱텍토' &&
+                window.location.replace('/tic-tac-toe');
+            }
+          }}
+        >
+          Retry
+        </button>
+        <button
+          className="mx-5 mr-1 mb-1 rounded bg-[#00917a] px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-[#00917a]"
           type="button"
           onClick={() => props.setShowModal(false)}
         >
